51614177529b1d4b728f517d3498478236255dec
Azhak Anwar <azhak.anwar@gmail.com>
2021-02-23 18:42:31 +0500

    Fixed static

--- rail/settings.py
+++ rail/settings.py
@@ -133,8 +133,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'staticfiles'),
@@ -150,11 +150,11 @@ STATICFILES_DIRS = (
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 # STATIC_ROOT = BASE_DIR / 'staticfiles' 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # The URL to use when referring to static files (where they will be served from)
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
