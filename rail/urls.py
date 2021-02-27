88c7031e4f2f62260d8bbbb2964c0ba813fe84e6
MuhammadSuleman97 <53903082+MuhammadSuleman97@users.noreply.github.com>
2021-02-27 18:28:40 +0500

    Update urls.py

--- rail/urls.py
+++ rail/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('register/',views.Reg,name="reg"),
     path('logout/',views.logout,name="logout"),
     path('home/',include('home.urls',namespace="home"),name="home"),
-    path('',views.login,name="login"),
+    path('',views.loginn,name="loginn"),
 
 
 ]
