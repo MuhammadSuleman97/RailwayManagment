a1b06d26c0eacc591b2784110756bdff61199462
MuhammadSuleman97 <53903082+MuhammadSuleman97@users.noreply.github.com>
2021-02-27 22:45:37 +0500

    Update settings.py

--- rail/settings.py
+++ rail/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = True
 
 # DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['suleman.pythonanywhere.com']
 
 
